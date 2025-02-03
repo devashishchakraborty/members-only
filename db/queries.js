682eef901ae0f6d752a22d8c43b3a7e4f9c18c5c
Devashish Chakraborty <devashishchakra@gmail.com>
2025-02-04 01:44:31 +0530

    Added queries file for data queries

--- db/queries.js
+++ db/queries.js
@@ -1,81 +1,66 @@
 import pool from "./pool.js";
 
-const getAllItems = async () => {
-  const { rows } = await pool.query("SELECT * FROM items");
+const getAllUsers = async () => {
+  const { rows } = await pool.query("SELECT * FROM users");
   return rows;
 };
 
-const getItem = async (id) => {
-  const { rows } = await pool.query("SELECT * FROM items WHERE id = $1", [id]);
+const getUser = async (id) => {
+  const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
   return rows[0];
 };
 
-const insertItem = async (item) => {
+const insertUser = async (user) => {
   await pool.query(
-    "INSERT INTO items (title, category, image_url, description, price) VALUES ($1, $2, $3, $4, $5)",
-    [item.title, item.category, item.image_url, item.description, item.price]
+    "INSERT INTO users (firstname, lastname, email, password) VALUES ($1, $2, $3, $4)",
+    [user.firstname, user.lastname, user.email, user.password]
   );
 };
 
-const updateItem = async (itemId, item) => {
+const updateUser = async (userId, user) => {
   await pool.query(
-    "UPDATE items SET title=$1, category=$2, image_url=$3, description=$4, price=$5 WHERE id=$6",
-    [
-      item.title,
-      item.category,
-      item.image_url,
-      item.description,
-      item.price,
-      itemId,
-    ]
+    "UPDATE users SET firstname=$1, lastname=$2, email=$3, password=$4 WHERE id=$6",
+    [user.firstname, user.lastname, user.email, user.password, userId]
   );
 };
 
-const deleteItem = async (id) => {
-  await pool.query("DELETE FROM items WHERE id = $1", [id]);
+const deleteUser = async (id) => {
+  await pool.query("DELETE FROM users WHERE id = $1", [id]);
 };
 
-const deleteAllItems = async () => {
-  await pool.query("DELETE FROM items");
-};
 
-const getItemsByCategory = async (categoryName) => {
-  const { rows } = await pool.query("SELECT * FROM items WHERE category = $1", [
-    categoryName,
-  ]);
+const getMessagesByCreatorId = async (creatorId) => {
+  const { rows } = await pool.query(
+    "SELECT * FROM messages WHERE creator_id = $1",
+    [creatorId]
+  );
   return rows;
 };
 
-const getAllCategories = async () => {
-  const { rows } = await pool.query("SELECT * FROM categories");
+const getAllMessages = async () => {
+  const { rows } = await pool.query("SELECT * FROM messages");
   return rows;
 };
 
-const insertCategory = async (categoryName) => {
-  await pool.query("INSERT INTO categories (name) VALUES ($1)", [categoryName]);
+const insertMessage = async (message, creatorId) => {
+  await pool.query(
+    "INSERT INTO messages (title, description, creator_id) VALUES ($1, $2, $3)",
+    [message.title, message.description, creatorId]
+  );
 };
 
-const updateCategory = async (newCategoryName, oldCategoryName) => {
-  await pool.query("UPDATE categories SET name = $1 WHERE name = $2", [
-    newCategoryName,
-    oldCategoryName,
-  ]);
-};
-
-const deleteCategory = async (categoryName) => {
-  await pool.query("DELETE FROM categories WHERE name=$1", [categoryName]);
+const deleteMessage = async (id) => {
+  await pool.query("DELETE FROM messages WHERE id=$1", [id]);
 };
 
 export default {
-  getItem,
-  getAllItems,
-  insertItem,
-  updateItem,
-  deleteItem,
-  deleteAllItems,
-  getAllCategories,
-  insertCategory,
-  getItemsByCategory,
-  deleteCategory,
-  updateCategory,
+  getUser,
+  getAllUsers,
+  insertUser,
+  updateUser,
+  deleteUser,
+  getAllMessages,
+  insertMessage,
+  getMessagesByCreatorId,
+  deleteMessage,
 };
