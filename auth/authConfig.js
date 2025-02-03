74140228a382f9542eb4f1cca3a4ac5b8bd66068
Devashish Chakraborty <devashishchakra@gmail.com>
2025-02-04 03:59:04 +0530

    Added authentication configuration for logins

---
+++ auth/authConfig.js
@@ -0,0 +1,34 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcryptjs";
+import queries from "../db/queries.js";
+
+passport.use(
+  new LocalStrategy(async (email, password, done) => {
+    try {
+      const user = await queries.getUserByEmail(email);
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect email" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await queries.getUser(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
